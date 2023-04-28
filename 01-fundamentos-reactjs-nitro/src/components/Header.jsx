65fe2119b180f8625c4c9ef62a4e6380b9bd34be
João Victor Ferreira Borba <60452036+J-Borba@users.noreply.github.com>
2023-04-28 18:09:30 -0300

    Update

--- 01-fundamentos-reactjs-nitro/src/components/Header.jsx
+++ 01-fundamentos-reactjs-nitro/src/components/Header.jsx
@@ -1,11 +1,16 @@
 import styles from './Header.module.css'
 
+import igniteLogo from '../assets/ignite-logo.svg'
+
 export function Header() {
 
     return (
         <header className={styles.header}>
 
-            <strong>Ignite Feed</strong>
+            <img src={igniteLogo} alt="Ignite Logo" />
+
+            <p>testando dnv</p>
+            {/* <strong>Ignite Feed</strong> */}
 
         </header>
     );
--- 01-fundamentos-reactjs-nitro/vite.config.js
+++ 01-fundamentos-reactjs-nitro/vite.config.js
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5000,
+    watch: {
+      usePolling: true
+    }
   }
 })
