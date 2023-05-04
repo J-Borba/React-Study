315dd68fc2e8d14e1f20d9bb1a0873c27966d463
João Victor Ferreira Borba <60452036+J-Borba@users.noreply.github.com>
2023-05-04 08:52:40 -0300

    Update

--- 02-ignite-timer/src/pages/History.tsx
+++ 02-ignite-timer/src/pages/History.tsx
@@ -1,7 +1,84 @@
+import {
+  HistoryContainer,
+  HistoryList,
+  Status,
+} from '../styles/HistoryPage.styles'
+
 export function History() {
   return (
-    <div>
-      <h1>History</h1>
-    </div>
+    <HistoryContainer>
+      <h1>Meu Histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluido </Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <Status statusColor="green"> Concluido </Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <Status statusColor="green"> Concluido </Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <Status statusColor="green"> Concluido </Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <Status statusColor="green"> Concluido </Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <Status statusColor="green"> Concluido </Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <Status statusColor="green"> Concluido </Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
   )
 }
--- 02-ignite-timer/src/pages/Home.tsx
+++ 02-ignite-timer/src/pages/Home.tsx
@@ -1,16 +1,54 @@
 import { Play } from 'phosphor-react'
-import { CountdownContainer, FormContainer, HomeContainer } from './Home.styles'
+import { useForm } from 'react-hook-form'
+
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StyledButton,
+  TaskInput,
+} from '../styles/HomePage.styles'
 
 export function Home() {
+  const { register, handleSubmit, watch } = useForm()
+
+  function handleNewCycle(data: any) {
+    console.log(data)
+  }
+
+  const isButtonDisabled = !watch('task') || !watch('minutesAmount')
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id="task" />
+
+          <TaskInput
+            id="task"
+            list="task-suggestion"
+            placeholder="Dê um nome para o seu projeto"
+            {...register('task')}
+          />
+
+          <datalist id="task-suggestion">
+            <option value="Estudar" />
+            <option value="Programar" />
+            <option value="Trabalhar" />
+          </datalist>
 
           <label htmlFor="">durante</label>
-          <input type="number" id="minutesAmount" />
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
+          />
 
           <span>minutos</span>
         </FormContainer>
@@ -18,15 +56,15 @@ export function Home() {
         <CountdownContainer>
           <span>0</span>
           <span>0</span>
-          <span>:</span>
+          <Separator>:</Separator>
           <span>0</span>
           <span>0</span>
         </CountdownContainer>
 
-        <button type="submit">
+        <StyledButton type="submit" disabled={isButtonDisabled}>
           <Play size={24} />
           Começar
-        </button>
+        </StyledButton>
       </form>
     </HomeContainer>
   )
